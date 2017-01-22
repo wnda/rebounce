319ad8098d76a8bb8c6e21e147b4668ea5387712
A. M. Douglas <me@amdouglas.com>
2017-01-22 17:45:18 +0000

    Update rebounce.js

--- rebounce.js
+++ rebounce.js
@@ -1,27 +1,26 @@
-function rebounce(f){
-  
+function rebounce (func) {
+  'use strict';
   var scheduled;
   var context;
   var args;
+  var i;
   
   return function(){
-  
     context = this;
     args = [];
+    i = 0;
     
-    for ( var i = 0; i < arguments.length; ++i ){
+    for (; i < arguments.length; ++i) {
       args[i] = arguments[i];
     }
     
-    if ( scheduled ){
-      window.cancelAnimationFrame( scheduled );
+    if (scheduled) {
+      window.cancelAnimationFrame(scheduled);
     }
     
-    scheduled = window.requestAnimationFrame( function(){
-      f.apply( context, args );
+    scheduled = window.requestAnimationFrame(function () {
+      func.apply(context, args);
       scheduled = null;
     });
-  
   }
-  
 }
